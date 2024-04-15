b03b0d6104de2b163009597a7adf323b8a424b19
aoh4pm <126729088+aoh4pm@users.noreply.github.com>
2024-04-15 13:04:34 -0700

    Update cat_facts.py

--- cat_facts.py
+++ cat_facts.py
@@ -5,6 +5,11 @@ class Cats:
     def __init__(self, base):
         self.base = base
 
+    def get_api_response(self):
+        endpoint_url = f"{self.base}"
+        r = requests.get(endpoint_url)
+        return r
+
     def get_random_fact(self):
         endpoint_url = f"{self.base}/fact"
         r = requests.get(endpoint_url)
